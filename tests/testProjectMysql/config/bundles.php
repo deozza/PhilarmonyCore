7b11cf4b61436e6e8cb32167dd771930fb13e958
Edenn <deozza@gmail.com>
2019-08-11 16:00:28 +0200

    fixed tests projects

--- tests/testProjectMysql/config/bundles.php
+++ tests/testProjectMysql/config/bundles.php
@@ -10,6 +10,7 @@ return [
     Deozza\PhilarmonyCoreBundle\DeozzaPhilarmonyCoreBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true, 'test'=>true],
